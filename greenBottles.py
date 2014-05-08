c1c60f097d2c4d4406c9fef0a10c27c343139e60
clush7113 <clush7113@sohamcollege.org.uk>
2014-05-08 11:53:51 +0100

    Update greenBottles.py
    Removed original code, used For loop for iteration and if statement to use correct pluralisation of bottle

--- greenBottles.py
+++ greenBottles.py
@@ -1,4 +1,16 @@
-print("10 green bottles, hanging on the wall")
-print("10 green bottles, hanging on the wall")
-print("And if 1 green bottle should acidentally fall,")
-print("They'll be 9 green bottles hanging on the wall.")
+import time
+
+def greenBottles():
+    bottles=int(input("How many green bottles? "))
+    for x in range(bottles):
+        if bottles-1==1:
+            plural="bottle"
+        else:
+            plural="bottles"
+        print("""{0} green bottles, sitting on the wall
+                 {0} green bottles, sitting on the wall
+                 but if 1 green bottle should accidentally fall
+                 there'll be {1} green {2} sitting on the wall""".format(bottles,bottles-1,plural))
+        bottles=bottles-1
+        time.sleep(0.5)
+greenBottles()
